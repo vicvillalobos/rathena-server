c242138ff2a9a1bbedcd949bc5a908b0277feced
Victor Villalobos <vpva.eien@gmail.com>
2025-05-18 19:56:33 -0400

    use update

--- tools/transform_costumes.py
+++ tools/transform_costumes.py
@@ -7,7 +7,7 @@
 import mysql.connector
 
 class ItemRenewal:
-    def __init__(self, id, name_aegis, name_english, type, subtype, price_buy, price_sell, weight, location_costume_head_top, location_costume_head_mid, location_costume_head_low, location_costume_garment, view):
+    def __init__(self, id, name_aegis, name_english, type, subtype, price_buy, price_sell, weight, location_costume_head_top, location_costume_head_mid, location_costume_head_low, location_costume_garment, view, equip_script, unequip_script):
         self.id = id
         self.name_aegis = name_aegis
         self.name_english = name_english
@@ -21,6 +21,8 @@ class ItemRenewal:
         self.location_costume_head_low = location_costume_head_low
         self.location_costume_garment = location_costume_garment
         self.view = view
+        self.equip_script = equip_script
+        self.unequip_script = unequip_script
         
     def __str__(self):
         return f"ItemRenewal (id={self.id}, name_aegis={self.name_aegis})"
@@ -28,8 +30,15 @@ class ItemRenewal:
     def generate_prerenewal_insert(self):
         # Generate the insert statement for the pre-renewal database
         return (
-            "INSERT INTO item_db2 (id, name_aegis, name_english, type, subtype, price_buy, price_sell, weight, location_costume_head_top, location_costume_head_mid, location_costume_head_low, location_costume_garment, view, armor_level) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 1);",
-            (self.id, self.name_aegis, self.name_english, self.type, self.subtype, self.price_buy, self.price_sell, self.weight, self.location_costume_head_top, self.location_costume_head_mid, self.location_costume_head_low, self.location_costume_garment, self.view)
+            "INSERT INTO item_db2 (id, name_aegis, name_english, type, subtype, price_buy, price_sell, weight, location_costume_head_top, location_costume_head_mid, location_costume_head_low, location_costume_garment, view, armor_level, equip_script, unequip_script) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 1);",
+            (self.id, self.name_aegis, self.name_english, self.type, self.subtype, self.price_buy, self.price_sell, self.weight, self.location_costume_head_top, self.location_costume_head_mid, self.location_costume_head_low, self.location_costume_garment, self.view, self.equip_script, self.unequip_script)
+        )
+        
+    def generate_prerenewal_update(self):
+        # Generate the update statement for the pre-renewal database
+        return (
+            "UPDATE item_db2 SET name_aegis = %s, name_english = %s, type = %s, subtype = %s, price_buy = %s, price_sell = %s, weight = %s, location_costume_head_top = %s, location_costume_head_mid = %s, location_costume_head_low = %s, location_costume_garment = %s, view = %s, equip_script = %s, unequip_script = %s WHERE id = %s;",
+            (self.name_aegis, self.name_english, self.type, self.subtype, self.price_buy, self.price_sell, self.weight, self.location_costume_head_top, self.location_costume_head_mid, self.location_costume_head_low, self.location_costume_garment, self.view, self.equip_script, self.unequip_script, self.id)
         )
     
     
@@ -59,10 +68,12 @@ try:
             item = ItemRenewal(*row)
             print(item)
             # Generate the insert statement for the pre-renewal database
-            insert_statement = item.generate_prerenewal_insert()
-            print("Inserting...")
-            # Execute the insert statement
-            mycursor.execute(insert_statement[0], insert_statement[1])
+            #insert_statement = item.generate_prerenewal_insert()
+            #print("Inserting...")
+            #mycursor.execute(insert_statement[0], insert_statement[1])
+            update_statement = item.generate_prerenewal_update()
+            print("Updating...")
+            mycursor.execute(update_statement[0], update_statement[1])
             # Commit the changes to the database
         except Exception as ee:
             print("Error while inserting data into MySQL:", ee)
