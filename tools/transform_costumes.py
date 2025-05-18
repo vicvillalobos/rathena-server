5499d6cf9d9202dc32a9b60ce81c905d56293540
Victor Villalobos <vpva.eien@gmail.com>
2025-05-18 19:19:34 -0400

    changed execution logic

--- tools/transform_costumes.py
+++ tools/transform_costumes.py
@@ -27,7 +27,10 @@ class ItemRenewal:
 
     def generate_prerenewal_insert(self):
         # Generate the insert statement for the pre-renewal database
-        return f"INSERT INTO item_db2 (id, name_aegis, name_english, type, subtype, price_buy, price_sell, weight, location_costume_head_top, location_costume_head_mid, location_costume_head_low, location_costume_garment, view, armor_level) VALUES ({self.id}, '{self.name_aegis}', '{self.name_english}', {self.type}, {self.subtype}, {self.price_buy}, {self.price_sell}, {self.weight}, {self.location_costume_head_top}, {self.location_costume_head_mid}, {self.location_costume_head_low}, {self.location_costume_garment}, {self.view}, 1);"
+        return (
+            "INSERT INTO item_db2 (id, name_aegis, name_english, type, subtype, price_buy, price_sell, weight, location_costume_head_top, location_costume_head_mid, location_costume_head_low, location_costume_garment, view, armor_level) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 1);",
+            (self.id, self.name_aegis, self.name_english, self.type, self.subtype, self.price_buy, self.price_sell, self.weight, self.location_costume_head_top, self.location_costume_head_mid, self.location_costume_head_low, self.location_costume_garment, self.view)
+        )
     
     
 
@@ -50,16 +53,22 @@ mycursor.execute("SELECT id, name_aegis, name_english, type, subtype, price_buy,
 myresult = mycursor.fetchall()
 
 # Print the first 10 rows
-for row in myresult:
-    item = ItemRenewal(*row)
-    print(item)
-    # Generate the insert statement for the pre-renewal database
-    insert_statement = item.generate_prerenewal_insert()
-    print(insert_statement)
-    # Execute the insert statement
-    mycursor.execute(insert_statement)
-    # Commit the changes to the database
-mydb.commit()
+try:
+    for row in myresult:
+        item = ItemRenewal(*row)
+        print(item)
+        # Generate the insert statement for the pre-renewal database
+        insert_statement = item.generate_prerenewal_insert()
+        print(insert_statement)
+        # Execute the insert statement
+        mycursor.execute(insert_statement[0], insert_statement[1])
+        # Commit the changes to the database
+        
+    mydb.commit()
+except Exception as e:
+    print("Error while inserting data into MySQL:", e)
+    # Rollback in case there is any error
+    mydb.rollback()
 # Print the number of rows inserted
 print(mycursor.rowcount, "rows inserted into item_db2")
     
