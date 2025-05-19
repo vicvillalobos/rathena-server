ceb21eec44fc2c7f7b54376a99cf849cd7914f81
Victor Villalobos <vpva.eien@gmail.com>
2025-05-18 20:04:21 -0400

    added missing script column

--- tools/transform_costumes.py
+++ tools/transform_costumes.py
@@ -7,7 +7,7 @@
 import mysql.connector
 
 class ItemRenewal:
-    def __init__(self, id, name_aegis, name_english, type, subtype, price_buy, price_sell, weight, location_costume_head_top, location_costume_head_mid, location_costume_head_low, location_costume_garment, view, equip_script, unequip_script):
+    def __init__(self, id, name_aegis, name_english, type, subtype, price_buy, price_sell, weight, location_costume_head_top, location_costume_head_mid, location_costume_head_low, location_costume_garment, view, script, equip_script, unequip_script):
         self.id = id
         self.name_aegis = name_aegis
         self.name_english = name_english
@@ -21,24 +21,25 @@ class ItemRenewal:
         self.location_costume_head_low = location_costume_head_low
         self.location_costume_garment = location_costume_garment
         self.view = view
+        self.script = script
         self.equip_script = equip_script
         self.unequip_script = unequip_script
         
     def __str__(self):
-        return f"ItemRenewal (id={self.id}, name_aegis={self.name_aegis}, equip_script={self.equip_script}, unequip_script={self.unequip_script})"
+        return f"ItemRenewal (id={self.id}, name_aegis={self.name_aegis})"
 
     def generate_prerenewal_insert(self):
         # Generate the insert statement for the pre-renewal database
         return (
-            "INSERT INTO item_db2 (id, name_aegis, name_english, type, subtype, price_buy, price_sell, weight, location_costume_head_top, location_costume_head_mid, location_costume_head_low, location_costume_garment, view, armor_level, equip_script, unequip_script) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 1);",
-            (self.id, self.name_aegis, self.name_english, self.type, self.subtype, self.price_buy, self.price_sell, self.weight, self.location_costume_head_top, self.location_costume_head_mid, self.location_costume_head_low, self.location_costume_garment, self.view, self.equip_script, self.unequip_script)
+            "INSERT INTO item_db2 (id, name_aegis, name_english, type, subtype, price_buy, price_sell, weight, location_costume_head_top, location_costume_head_mid, location_costume_head_low, location_costume_garment, view, armor_level, script, equip_script, unequip_script) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 1);",
+            (self.id, self.name_aegis, self.name_english, self.type, self.subtype, self.price_buy, self.price_sell, self.weight, self.location_costume_head_top, self.location_costume_head_mid, self.location_costume_head_low, self.location_costume_garment, self.view, self.script, self.equip_script, self.unequip_script)
         )
         
     def generate_prerenewal_update(self):
         # Generate the update statement for the pre-renewal database
         return (
-            "UPDATE item_db2 SET name_aegis = %s, name_english = %s, type = %s, subtype = %s, price_buy = %s, price_sell = %s, weight = %s, location_costume_head_top = %s, location_costume_head_mid = %s, location_costume_head_low = %s, location_costume_garment = %s, view = %s, equip_script = %s, unequip_script = %s WHERE id = %s;",
-            (self.name_aegis, self.name_english, self.type, self.subtype, self.price_buy, self.price_sell, self.weight, self.location_costume_head_top, self.location_costume_head_mid, self.location_costume_head_low, self.location_costume_garment, self.view, self.equip_script, self.unequip_script, self.id)
+            "UPDATE item_db2 SET name_aegis = %s, name_english = %s, type = %s, subtype = %s, price_buy = %s, price_sell = %s, weight = %s, location_costume_head_top = %s, location_costume_head_mid = %s, location_costume_head_low = %s, location_costume_garment = %s, view = %s, script = %s, equip_script = %s, unequip_script = %s WHERE id = %s;",
+            (self.name_aegis, self.name_english, self.type, self.subtype, self.price_buy, self.price_sell, self.weight, self.location_costume_head_top, self.location_costume_head_mid, self.location_costume_head_low, self.location_costume_garment, self.view, self.script, self.equip_script, self.unequip_script, self.id)
         )
     
     
@@ -56,7 +57,7 @@ mycursor = mydb.cursor()
 mycursor.execute("USE ragnarok")
 
 # Select all costume items from renewal database
-mycursor.execute("SELECT id, name_aegis, name_english, type, subtype, price_buy, price_sell, weight, location_costume_head_top, location_costume_head_mid, location_costume_head_low, location_costume_garment, view, equip_script, unequip_script FROM `item_db_re` WHERE location_costume_head_top IS NOT NULL OR location_costume_head_mid IS NOT NULL OR location_costume_head_low IS NOT NULL OR location_costume_garment IS NOT NULL")
+mycursor.execute("SELECT id, name_aegis, name_english, type, subtype, price_buy, price_sell, weight, location_costume_head_top, location_costume_head_mid, location_costume_head_low, location_costume_garment, view, script, equip_script, unequip_script FROM `item_db_re` WHERE location_costume_head_top IS NOT NULL OR location_costume_head_mid IS NOT NULL OR location_costume_head_low IS NOT NULL OR location_costume_garment IS NOT NULL")
 
 # Fetch all rows
 myresult = mycursor.fetchall()
